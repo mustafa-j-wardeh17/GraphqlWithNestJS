a8908d74ebc382b1b4ce874684e47c4646ac1534
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-18 15:39:58 +0300

    First query within the resolver

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -8,28 +8,17 @@ import { UpdatePetInput } from './dto/update-pet.input';
 export class PetsResolver {
   constructor(private readonly petsService: PetsService) {}
 
-  @Mutation(() => Pet)
-  createPet(@Args('createPetInput') createPetInput: CreatePetInput) {
-    return this.petsService.create(createPetInput);
-  }
 
-  @Query(() => [Pet], { name: 'pets' })
-  findAll() {
+  @Query(() => [Pet])
+  pets():Promise<Pet[]> {
     return this.petsService.findAll();
   }
 
-  @Query(() => Pet, { name: 'pet' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.petsService.findOne(id);
-  }
 
-  @Mutation(() => Pet)
-  updatePet(@Args('updatePetInput') updatePetInput: UpdatePetInput) {
-    return this.petsService.update(updatePetInput.id, updatePetInput);
-  }
 
-  @Mutation(() => Pet)
-  removePet(@Args('id', { type: () => Int }) id: number) {
-    return this.petsService.remove(id);
-  }
+
+  // @Mutation(() => Pet)
+  // removePet(@Args('id', { type: () => Int }) id: number) {
+  //   return this.petsService.remove(id);
+  // }
 }

c289a6a23807380299cea4d7b1adbf5906b2e4a0
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-18 15:36:35 +0300

    create pet object type

--- src/pets/entities/pet.entity.ts
+++ src/pets/entities/pet.entity.ts
@@ -2,6 +2,14 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Pet {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(type => Int) // Scalar type for graphql: Int to make id as int number style
+  id: number;
+
+
+  @Field()
+  name: string;
+
+
+  @Field({ nullable: true }) // you still have to make this graphql nullable to make property optional
+  type?: string;
 }
--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePetInput } from './dto/create-pet.input';
 import { UpdatePetInput } from './dto/update-pet.input';
+import { Pet } from './entities/pet.entity';
 
 @Injectable()
 export class PetsService {
-  create(createPetInput: CreatePetInput) {
-    return 'This action adds a new pet';
-  }
 
-  findAll() {
-    return `This action returns all pets`;
-  }
+  async findAll(): Promise<Pet[]> {
+    const pet = new Pet();
+    pet.id = 1;
+    pet.name = 'Mambo';
 
-  findOne(id: number) {
-    return `This action returns a #${id} pet`;
-  }
-
-  update(id: number, updatePetInput: UpdatePetInput) {
-    return `This action updates a #${id} pet`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} pet`;
+    return [pet]
   }
 }

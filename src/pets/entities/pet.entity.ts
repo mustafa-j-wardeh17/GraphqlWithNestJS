0b4125a84a91ad081629f6d7b5e2823227e8e22d
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-18 16:01:54 +0300

    Pet database entity

--- src/pets/entities/pet.entity.ts
+++ src/pets/entities/pet.entity.ts
@@ -1,16 +1,18 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
-
+@Entity() // To make the entity inside pg db
+@ObjectType() // to contact with graphql resolvers will
 export class Pet {
+  @PrimaryGeneratedColumn()
   @Field(type => Int) // Scalar type for graphql: Int to make id as int number style
   id: number;
 
-
+  @Column()
   @Field()
   name: string;
 
-
+  @Column({ nullable: true })
   @Field({ nullable: true }) // you still have to make this graphql nullable to make property optional
   type?: string;
 }

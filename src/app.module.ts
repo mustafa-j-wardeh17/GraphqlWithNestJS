b6a97e76fd86e6a34bafaaec6c6a5110208576f7
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-18 15:28:55 +0300

    Graph Module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // Code first methodology
+      // typePaths: ['./**/*.graphql'], //For schema first
     }),
+    PetsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
